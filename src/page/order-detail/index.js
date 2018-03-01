85bf85ad932320940f0642abf3223a70abf576f1
wangxiaobo <1124683115@qq.com>
2018-03-01 12:04:06 +0800

    payment

--- src/page/order-detail/index.js
+++ src/page/order-detail/index.js
@@ -2,9 +2,9 @@
 require('./index.css');
 require('page/common/nav/index.js');
 require('page/common/header/index.js');
-var _mm = require('util/mm.js');
-var _order = require('service/order-service.js');
-var navSide = require('page/common/nav-side/index.js');
+var _mm           = require('util/mm.js');
+var _order        = require('service/order-service.js');
+var navSide       = require('page/common/nav-side/index.js');
 var templateIndex = require('./index.string');
 
 var page = {
--- src/page/result/index.js
+++ src/page/result/index.js
@@ -4,5 +4,10 @@ require('page/common/nav-simple/index.js');
 var _mm=require('util/mm.js');
 $(function () {
    var type=_mm.getUrlParam('type')||'default',
-      $element=$('.'+type+'-success').show()
+      $element=$('.'+type+'-success').show();
+   if(type === 'payment'){
+      var orderNumber = _mm.getUrlParam('orderNumber'),
+         $orderNumber = $element.find('.order-number');
+      $orderNumber.attr('href',$orderNumber.attr('href') + orderNumber);
+   }
 })
